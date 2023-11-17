c5e13df524fa8afc19ff59925ef973baa3d86fe8
Shrimpy <s3978724@rmit.edu.vn>
2023-11-17 19:02:51 +0700

    working on button

--- src/lib/components/controls/Button.svelte
+++ src/lib/components/controls/Button.svelte
@@ -1,11 +1,25 @@
 <script>
     import Body from "$lib/components/typography/Body.svelte";
-    export let text;
+    import Fa from "svelte-fa";
+    import {faCog} from "@fortawesome/pro-regular-svg-icons";
+    export let text = "Text";
+    export const State = {
+        Enabled: "ENABLED",
+        Disabled: "DISABLED"
+    }
+    export const Style = {
+        Outlined: "OUTLINED",
+        Default: "DEFAULT"
+    }
+    export let state = State.Enabled;
+    export let style = Style.Outlined;
 </script>
 
-<button class="px-5 py-3 rounded-full border border-white border-opacity-30 backdrop-blur-sm justify-center items-center gap-2 inline-flex">
-    <div class="">
-
-    </div>
+<button class="px-5 py-3 rounded-full border border-white border-opacity-30 backdrop-blur-sm justify-center items-center gap-2 inline-flex
+        {state === State.Enabled ? 'text-opacity-50' : ''}
+        {style === Style.Default ? 'bg-white text-gray-900' : ''}">
+<Fa icon={faCog}
+    size="${16}x"
+    color="{style === Style.Default ? 'black' : 'white'}" />
     <Body {text}></Body>
 </button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 <h1 class="text-3xl font-bold underline text-red-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<Button text = "a"></Button>
+<Button text={"Button"}></Button>
+<Button state={State.Disabled} style={Style.Outlined} text={"Button"}></Button>
 
 <p>Hello</p>
