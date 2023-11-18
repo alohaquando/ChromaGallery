7f9e800efc14b21354ed340c2d398add5f19ba50
Shrimpy <s3978724@rmit.edu.vn>
2023-11-18 13:04:25 +0700

    finishing button

--- src/lib/components/controls/Button.svelte
+++ src/lib/components/controls/Button.svelte
@@ -15,11 +15,11 @@
     export let style = Style.Outlined;
 </script>
 
-<button class="px-5 py-3 rounded-full border border-white border-opacity-30 backdrop-blur-sm justify-center items-center gap-2 inline-flex line-clamp-1 overflow-visible ease-out duration-3a00
-        {state === State.Enabled ? 'hover:text-black hover:before:opacity-70 hover:after:opacity-70' : 'text-white/50'}
-        {style === Style.Default ? 'bg-white text-gray-900 hover:bg-white/60' : 'hover:bg-white/40'}
-        after:content-[''] after:h-full after:w-full after:rounded-full after:absolute after:top-auto after:bg-gradient-to-b from-white/0 to-white after:blur-md after:opacity-0 after:duration-1000 after:ease-out after:-z-10
-        before:content-[''] before:h-full before:w-full before:bg-gradient-to-b from-white/20 via-white/40 to-white/80 before:rounded-full before:blur-md before:absolute before:opacity-0 before:duration-1000 before:ease-out before:-z-10
+<button class="px-5 py-3 rounded-full backdrop-blur-sm justify-center items-center gap-2 inline-flex line-clamp-1 overflow-visible ease-out duration-300
+        {state === State.Enabled ? 'hover:bg-white/60 hover:text-gray-900 hover:before:opacity-70 hover:after:opacity-70 hover:font-' : 'text-white/50'}
+        {style === Style.Default ? 'bg-white text-gray-900 active:bg-white/80' : 'border border-white border-opacity-30'}
+        after:content-[''] after:h-full after:w-full after:rounded-full after:absolute after:top-auto after:bg-gradient-to-b from-white/0 to-white after:blur-lg after:opacity-0 after:duration-1000 after:ease-out after:-z-10
+        before:content-[''] before:h-full before:w-full before:bg-gradient-to-b from-white/20 via-white/40 to-white/80 before:rounded-full before:blur-lg before:absolute before:opacity-0 before:duration-1000 before:ease-out before:-z-10
 ">
 <Fa icon={faCog} class=""
     size="${16}x"
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,13 @@
 <script>
     import Button from "$lib/components/controls/Button.svelte";
+    import Chip from "$lib/components/controls/Chip.svelte";
 </script>
 
 <h1 class="text-3xl font-bold underline text-red-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
+<Button state={"DISABLED"} text={"Button"}></Button>
+<Button style={"DEFAULT"} text={"Button"}></Button>
 <Button text={"Button"}></Button>
-<Button state={"ENABLED"} style={"DEFAULT"} text={"Button"}></Button>
-<Button state={"DISABLED"} style={"OUTLINED"} text={"Button"}></Button>
 
 <p>Hello</p>
