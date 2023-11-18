872477d8cc6c580c1bd213e2a377c728c12d3171
Shrimpy <s3978724@rmit.edu.vn>
2023-11-18 20:56:15 +0700

    Chips

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,22 @@
 <script>
     import Button from "$lib/components/controls/Button.svelte";
     import Chip from "$lib/components/controls/Chip.svelte";
+    import { Fa } from "svelte-fa";
 </script>
 
 <h1 class="text-3xl font-bold underline text-red-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
+<div>Buttons</div>
+
 <Button state={"DISABLED"} text={"Button"}></Button>
 <Button style={"DEFAULT"} text={"Button"}></Button>
 <Button text={"Button"}></Button>
+
+<div><span class="line-through">Fish&</span>Chips</div>
+
+<Chip type={"VIEW-ONLY"} text={"Chip"}></Chip>
+<Chip state={"ACTIVE"} text={"Chip"}></Chip>
+<Chip text={"Chip"}></Chip>
 
 <p>Hello</p>
