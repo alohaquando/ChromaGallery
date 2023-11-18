f82d2afabdf1988aad5055c68551270c816853d6
Shrimpy <s3978724@rmit.edu.vn>
2023-11-18 21:06:56 +0700

    Chips

--- src/lib/components/controls/Chip.svelte
+++ src/lib/components/controls/Chip.svelte
@@ -2,6 +2,7 @@
   import Body from "$lib/components/typography/Body.svelte";
   import Fa from "svelte-fa";
   import {faCircleInfo} from "@fortawesome/pro-regular-svg-icons";
+  import {faCircleInfo as solidCircleInfo} from "@fortawesome/pro-solid-svg-icons"
   import { elasticIn } from "svelte/easing";
   export let text = "Text";
   const Type = {
@@ -23,6 +24,10 @@
         after:content-[''] after:h-full after:w-full after:rounded-full after:absolute after:top-auto after:bg-gradient-to-b from-white/0 to-white after:blur-lg after:opacity-0 after:duration-1000 after:ease-out after:-z-10
         before:content-[''] before:h-full before:w-full before:bg-gradient-to-b from-white/20 via-white/40 to-white/80 before:rounded-full before:blur-lg before:absolute before:opacity-0 before:duration-1000 before:ease-out before:-z-10
 ">
+  {#if state === State.Active}
+    <Fa icon={solidCircleInfo} size="${16}x"/>
+    {:else}
   <Fa icon={faCircleInfo} size="${16}x"/>
+    {/if}
   <Body {text}></Body>
 </button>
