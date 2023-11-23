62447bde77cdaed5eb8da487e316d466c407f2fb
Tin <93732406+Tin0709@users.noreply.github.com>
2023-11-23 21:24:56 +0700

    OMW55

--- src/lib/components/pages/Background2.svelte
+++ src/lib/components/pages/Background2.svelte
@@ -1,7 +1,7 @@
 <div
-	class="h-[905px] w-[390px] bg-[linear-gradient(_0deg,hsl(0deg_0%_0%)_0%,hsl(241deg_100%_15%)_30%,hsl(248deg_100%_17%)_100%_)] relative overflow-hidden;"
+	class="h-[905px] w-[390px] bg-[linear-gradient(_0deg,hsl(0deg_0%_0%)_0%,hsl(241deg_100%_15%)_30%,hsl(248deg_100%_17%)_100%_)] relative overflow-hidden flex;"
 >
 	<div
-		class="h-[200px] w-[200px] bg-transparent -translate-x-2/4 -translate-y-[-150%] bg-[linear-gradient(transparent,#b61aff)] absolute blur-[15px] rounded-[50%] left-2/4 top-2/4;"
+		class="h-[200px] w-[200px] bg-transparent -translate-x-2/4 -translate-y-2/4 bg-[linear-gradient(transparent,#b61aff)] absolute blur-[15px] rounded-[50%] left-2/4 top-2/4 flex;"
 	></div>
 </div>
--- src/lib/components/pages/Background4.svelte
+++ src/lib/components/pages/Background4.svelte
@@ -1,5 +1,5 @@
 <div
-	class="h-[905px] w-[390px] bg-[linear-gradient(_0deg,hsl(0deg_0%_0%)_0%,hsl(241deg_100%_15%)_30%,hsl(248deg_100%_17%)_100%_)] relative overflow-hidden;"
+	class="h-[905px] w-[390px] bg-[linear-gradient(_0deg,hsl(0deg_0%_0%)_0%,hsl(241deg_100%_15%)_30%,hsl(248deg_100%_17%)_100%_)] relative overflow-hidden flex;"
 >
 	<div
 		class="h-[600px] w-[600px] bg-transparent -translate-x-2/4 -translate-y-[15%] bg-[linear-gradient(#ffffff1b,#b61aff)] absolute blur-[25px] rounded-[50%] left-2/4 top-[18%];"
