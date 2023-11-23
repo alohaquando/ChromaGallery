cfb2e1c3c608fb7b39ab7609b416c870a06494b3
alohaquando <quan@quanhoangdo.com>
2023-11-23 19:08:49 +0700

    Installed tailwindcss-leading-trim

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,24 @@ export default {
     content: ['./src/**/*.{html,js,svelte,ts}'],
     theme: {
       fontFamily: {
-        sans: ['Atkinson Hyperlegible, system-ui'],
-        display: ['Outfit, system-ui']
+        sans: ['Atkinson Hyperlegible', 'system-ui'],
+        display: ['Outfit', 'system-ui']
+      },
+      fontMetrics: {
+        "Atkinson Hyperlegible": {
+          capHeight: 660,
+          ascent: 796,
+          descent: -251,
+          unitsPerEm: 1000,
+        },
+        "Outfit": {
+          capHeight: 694,
+          ascent: 1000,
+          descent: -260,
+          unitsPerEm: 1000,
+        }
       },
       extend: {}
     },
-    plugins: []
+    plugins: [require("tailwindcss-leading-trim")]
   };
