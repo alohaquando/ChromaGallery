0ab5a51f220f810a42c24183ae62b036d9773dac
Tin <93732406+Tin0709@users.noreply.github.com>
2023-11-24 17:36:11 +0700

    OMW-56

---
+++ src/lib/components/controls/Radio.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let id = '';
+	export let name = '';
+	export let value = '';
+</script>
+
+<div class="w-5 h-5 relative flex">
+	<input type="radio" {id} {name} {value} class="hidden" />
+	<label for={id} class="h-full w-full bg-white rounded-full cursor-pointer group-checked"></label>
+</div>
+
+<label for={id}>
+	<input type="radio" {id} {name} {value} class="hidden peer" />
+	<div class="w-5 h-5 relative flex bg-white peer-checked:bg-red-600"></div>
+</label>
