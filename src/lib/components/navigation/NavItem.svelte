5df404890aace7ebc9bd6ea798d388002778a251
Quân Đỗ <aloha.quando@gmail.com>
2023-11-25 19:33:21 +0700

    Added width to navItem to align them

--- src/lib/components/navigation/NavItem.svelte
+++ src/lib/components/navigation/NavItem.svelte
@@ -24,7 +24,7 @@
 
 <a
 	{href}
-	class="{customClasses} {activeClasses} px-3 py-2 flex-col justify-center items-center gap-1 inline-flex line-clamp-1 overflow-visible ease-out duration-300 text-white text-sm text-center relative w-fit space-y-0.5
+	class="{customClasses} {activeClasses} px-3 py-2 w-20 flex-col justify-center items-center gap-1 inline-flex line-clamp-1 overflow-visible ease-out duration-300 text-white text-sm text-center relative space-y-0.5
         after:content-[''] after:h-16 after:w-16 after:rounded-full after:absolute after:bg-gradient-to-b from-white/20 to-white/40 after:duration-1000 after:ease-out after:-z-10 after:blur-lg
 ">
 	<Icon {icon} size="xl"></Icon>
