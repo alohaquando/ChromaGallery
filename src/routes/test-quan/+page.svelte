1d516ef4f245158a6b02e9d692fd69416320694d
Quân Đỗ <aloha.quando@gmail.com>
2023-11-25 19:27:08 +0700

    Updated testing page for Quan

---
+++ src/routes/test-quan/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Body from '$lib/components/typography/Body.svelte';
+	import Link from '$lib/components/links/Link.svelte';
+</script>
+
+<div class="flex flex-col space-y-8">
+<Body>Heloo 123456
+<Link type="body">Testing</Link>
+</Body>
+<Link type="title">Testing</Link>
+<Link type="headline">Testing</Link>
+<Link type="display">Testing</Link>
+	<Body>Heloo 123456
+	<Link type="body">Testing</Link>
+	</Body>
+	<Link type="title">Testing</Link>
+	<Link type="headline">Testing</Link>
+	<Link type="display">Testing</Link>
+	<Body>Heloo 123456
+	<Link type="body">Testing</Link>
+	</Body>
+	<Link type="title">Testing</Link>
+	<Link type="headline">Testing</Link>
+	<Link type="display">Testing</Link><Body>Heloo 123456
+<Link type="body">Testing</Link>
+</Body>
+	<Link type="title">Testing</Link>
+	<Link type="headline">Testing</Link>
+	<Link type="display">Testing</Link>
+
+</div>
