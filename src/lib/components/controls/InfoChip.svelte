1db9dc10d9e4e8122806cf3216a42ab0d63cf24f
Quân Đỗ <aloha.quando@gmail.com>
2023-11-25 16:46:19 +0700

    Updated Chip

---
+++ src/lib/components/controls/InfoChip.svelte
@@ -0,0 +1,37 @@
+<!--suppress JSUnreachableSwitchBranches -->
+<script lang="ts">
+	import BodySmall from '$lib/components/typography/BodySmall.svelte';
+	import Icon from '$lib/components/iconography/Icon.svelte';
+
+	export let icon: string | undefined = undefined;
+	export let width: 'fit' | 'full' = 'fit';
+	let textClass = '';
+	let customClasses = '';
+	export { customClasses as class };
+
+	let widthClasses: string;
+	switch (width) {
+		case 'fit': {
+			widthClasses = 'w-fit';
+			break;
+		}
+		case 'full': {
+			widthClasses = 'w-full';
+			break;
+		}
+	}
+</script>
+
+<div
+	class="{customClasses} {widthClasses} px-3 py-2  backdrop-blur-sm justify-center items-center gap-1.5 inline-flex line-clamp-1 overflow-visible ease-out duration-300 text-sm text-center leading-[14px]
+
+	bg-gray-900 rounded-lg text-white
+
+   after:content-[''] after:h-full after:w-full after:rounded-full after:absolute after:top-auto after:bg-gradient-to-b after:blur-md after:opacity-0 after:duration-1000 after:ease-out after:-z-10
+   before:content-[''] before:h-full before:w-full before:bg-gradient-to-b from-white/0 via-white/40 to-white before:rounded-full before:blur-md before:absolute before:opacity-0 before:duration-1000 before:ease-out before:-z-10
+">
+	<Icon {icon} size="sm"></Icon>
+	<BodySmall class="{textClass}">
+		<slot />
+	</BodySmall>
+</div>
