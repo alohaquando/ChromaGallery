f3b7bc3383ce8392be0813631cd523e35ffa6c18
Quân Đỗ <aloha.quando@gmail.com>
2023-11-25 19:28:14 +0700

    Added NavBar and updated NavItem

--- src/lib/components/navigation/NavItem.svelte
+++ src/lib/components/navigation/NavItem.svelte
@@ -7,6 +7,7 @@
 	export { customClasses as class };
 	export let icon: string | undefined = undefined;
 	export let active: boolean = false;
+	export let href: string | undefined = undefined;
 
 	let activeClasses: string;
 	switch (active) {
@@ -15,13 +16,14 @@
 			break;
 		}
 		case false: {
-			activeClasses = 'opacity-60 after:hidden';
+			activeClasses = 'opacity-60 hover:opacity-100 after:hidden';
 			break;
 		}
 	}
 </script>
 
-<button
+<a
+	{href}
 	class="{customClasses} {activeClasses} px-3 py-2 flex-col justify-center items-center gap-1 inline-flex line-clamp-1 overflow-visible ease-out duration-300 text-white text-sm text-center relative w-fit space-y-0.5
         after:content-[''] after:h-16 after:w-16 after:rounded-full after:absolute after:bg-gradient-to-b from-white/20 to-white/40 after:duration-1000 after:ease-out after:-z-10 after:blur-lg
 ">
@@ -29,4 +31,4 @@
 	<Body class="{textClass}">
 	<slot />
 	</Body>
-</button>
+</a>
