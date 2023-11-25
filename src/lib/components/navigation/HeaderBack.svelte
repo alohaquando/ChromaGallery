7a4238e3bb4190efb99902993f50f39b25140993
Quân Đỗ <aloha.quando@gmail.com>
2023-11-25 19:28:29 +0700

    Updated Header styling

--- src/lib/components/navigation/HeaderBack.svelte
+++ src/lib/components/navigation/HeaderBack.svelte
@@ -35,7 +35,7 @@
 </script>
 
 <div
-	class="{designClass} sticky top-0 left-0 z-50 h-16 w-full backdrop-blur-3xl justify-between items-center inline-flex duration-500 ease-in-out">
+	class="{designClass} sticky top-0 left-0 z-50 h-16 mb-2 w-full  justify-between items-center inline-flex duration-500 ease-in-out">
 
 	<div class="flex container mx-auto px-6 justify-between items-center">
 		<Fab class="{buttonClass}" icon="faAngleLeft" size="sm"></Fab>
@@ -44,5 +44,11 @@
 				<slot />
 			</Button>
 		{/if}
+	</div>
+
+	<div
+		class="pointer-events-none absolute top-0 -bottom-2 left-0 right-0 -z-20 touch-none transition-all">
+		<div
+			class="opacity-100 blur-fix absolute h-full w-full backdrop-blur-lg [mask-image:linear-gradient(to_bottom,black,black,black,transparent)]" />
 	</div>
 </div>
