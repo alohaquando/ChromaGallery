8230ce549f0192ad573432d193422dc4fa13126d
Quân Đỗ <aloha.quando@gmail.com>
2023-11-25 19:25:27 +0700

    Moved backend nav out

---
+++ src/lib/wip/backend/+layout_old.svelte
@@ -0,0 +1,7 @@
+<nav>
+	<a class=" text-blue-500" href="/">home</a>
+	<a class=" text-green-500" href="/about">about</a>
+	<a class=" text-yellow-500" href="/dashboard">dashboard</a>
+	<h1>{userName}</h1>
+	<h1>{userEmail}</h1>
+</nav>
