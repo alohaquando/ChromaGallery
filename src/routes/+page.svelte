e3844777e88c990d40ed8467b730413b69c0e3e5
Shrimpy <s3978724@rmit.edu.vn>
2023-11-26 22:05:13 +0700

    Homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,17 @@
 <script>
-	import Link from '$lib/components/links/Link.svelte';
+	import DisplayLarge from '$lib/components/typography/DisplayLarge.svelte';
+	import Fab from '$lib/components/controls/Fab.svelte';
 </script>
 
-<h1 class="text-3xl font-bold underline text-red-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<Link href="/test-kiet">Test page here.</Link>
+<!--Replace div -> Background-->
+<div class="w-full h-xl pt-16 flex flex-col items-center gap-10">
+	<DisplayLarge class="relative -left-4">The place</DisplayLarge>
+	<DisplayLarge class="relative -left-16">for</DisplayLarge>
+	<DisplayLarge class="relative left-10">all things</DisplayLarge>
+	<DisplayLarge class="relative -left-1">artistic</DisplayLarge>
+	<div class="flex w-full justify-between mt-16">
+		<Fab class="mt-16" icon="faUser" size="lg">Sign me in</Fab>
+		<Fab class="" icon="faSearch" size="lg">Show me more</Fab>
+		<Fab icon="faPlay" size="lg">Relax me</Fab>
+	</div>
+</div>
