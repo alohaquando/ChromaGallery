1991684542e323db13d44ede884b748bb2db5e6f
Quân Đỗ <aloha.quando@gmail.com>
2023-11-27 23:11:25 +0700

    Added Google Logo and updated Social Button

--- src/lib/components/socials/Socials.svelte
+++ src/lib/components/socials/Socials.svelte
@@ -1,52 +1,62 @@
 <script lang="ts">
 	import Body from '$lib/components/typography/Body.svelte';
 	import Icon from '$lib/components/iconography/Icon.svelte';
-	import Button from '$lib/components/controls/Button.svelte';
+	import Google from '$lib/assets/svgs/Google.svelte';
 
-    let customClasses = '';
+	let customClasses = '';
 	export { customClasses as class };
-	export let href= '';
+	export let href = '';
 	export let design: 'facebook' | 'google' | 'apple';
-    export let signup: boolean= true;
-    
-    let bg: string, text: string, icon: string, tc: string;
+	export let signup: boolean = true;
+
+	let bg: string, text: string, icon: string, tc: string;
 	switch (design) {
 		case 'facebook': {
-            bg = 'bg-blue-600 hover:bg-blue-700 active:bg-blue-800';
-            text = ' Facebook'
-            icon = 'faFacebook'
+			bg = 'bg-blue-600 hover:bg-blue-700 active:bg-blue-800';
+			text = 'Facebook';
+			icon = 'faFacebook';
 			tc = 'text-white';
 			break;
 		}
 		case 'google': {
-            bg = 'bg-white hover:bg-neutral-200 active:bg-neutral-300'
-            text = ' Google'
-            icon = 'faGoogle'
+			bg = 'bg-white hover:bg-neutral-200 active:bg-neutral-300';
+			text = 'Google';
+			icon = 'faGoogle';
 			tc = 'text-black text-opacity-60';
-            break;
+			break;
 		}
-        case 'apple': {
-            bg = 'bg-black hover:bg-neutral-900 active:bg-neutral-800'
-            text = ' Apple'
-            icon = 'faApple'
+		case 'apple': {
+			bg = 'bg-black hover:bg-neutral-900 active:bg-neutral-800 outline outline-white/20';
+			text = 'Apple';
+			icon = 'faApple';
 			tc = 'text-white';
-            break;
+			break;
 		}
 	}
-    let txt: string;
-    if (signup){
-        txt = 'Sign up with'
-    }
-    else {
-        txt = 'Continue with'
-    }
+	let txt: string;
+	if (signup) {
+		txt = 'Sign up with';
+	} else {
+		txt = 'Continue with';
+	}
 </script>
 
 
-<button>
-    <div class="px-6 py-3 {bg} rounded-[99px] shadow justify-start items-center gap-4 inline-flex">
-    <Icon type="brands" {icon} class={tc} size="2xl"></Icon>
-    <Body class="grow shrink basis-0 text-center {tc} font-bold">{txt}{text}</Body>
-    </div>
-  </button>
+<svelte:element
+	this={href? 'a':'button'}
+	on:click
+	on:keydown
+	tabindex="0"
+	role="button"
+	class="px-6 h-12 rounded-[99px] shadow justify-start items-center gap-4 inline-flex transition {bg} {customClasses}">
+
+	{#if design === 'google'}
+		<div class="text-xl w-6 flex justify-center">
+			<Google />
+		</div>
+	{:else }
+		<Icon class={tc} {icon} size="2xl" type="brands"></Icon>
+	{/if}
+	<Body class="grow shrink basis-0 text-center {tc}">{txt} {text}</Body>
+</svelte:element>
   
