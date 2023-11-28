2292b9332972a0414c78b2d8cddd06bcb35eefbf
Tin <93732406+Tin0709@users.noreply.github.com>
2023-11-28 16:15:35 +0700

    OMW-81-Details

--- src/routes/details/+page.svelte
+++ src/routes/details/+page.svelte
@@ -3,6 +3,7 @@
 	import ListItem from '$lib/components/item/ListItem.svelte';
 	import Fab from '$lib/components/controls/Fab.svelte';
 	import Button from '$lib/components/controls/Button.svelte';
+	import BodyLarge from '$lib/components/typography/BodyLarge.svelte';
 </script>
 
 <HeroImage
@@ -23,8 +24,8 @@
 	<Fab icon="faStar"></Fab>
 </div>
 
-<div class=" text-white text-lg font-normal font-['Atkinson Hyperlegible'] leading-[27px] my-4">
-	This ball sports a distinctive stripe pattern that results from an innovative manufacturing
+<BodyLarge
+	>This ball sports a distinctive stripe pattern that results from an innovative manufacturing
 	process. Plastic of two colours is blown into a mould using a method that prevents the colours
 	from mixing. The ball is a by-product of a demonstration of the extrusion blow-mould machine
 	invented by the Hong Kong engineer Chiang Chen. The development of the two-colour production
@@ -32,14 +33,16 @@
 	the confluence of the Yangtze River and its tributary, the Jialing, in mainland China: one was
 	muddy and the other clear, but the two currents remained distinct rather than blending together.
 	Despite the success of his manufacturing method, Chiang was never able to file a patent for it,
-	and it was soon widely imitated to create other types of products.
-</div>
+	and it was soon widely imitated to create other types of products.</BodyLarge
+>
 
 <ListItem leadingText="Object ID" trailingText="HEE-NC-57001" />
 <ListItem leadingText="Place made" trailingText="Vietnam, Ho Chi Minh City" />
 <ListItem leadingText="Object IDNamed collection" trailingText="Harold E. Edgerton Collection" />
 
-<div class="text-white text-xl font-normal font-['Atkinson Hyperlegible'] leading-[30px]">
+<div
+	class="text-white text-xl font-normal font-['Atkinson Hyperlegible'] leading-[30px] my-[32px] inline-block align-middle"
+>
 	Related
 </div>
 
