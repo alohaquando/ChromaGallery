98aadd811f5b59ea7e1bc9177b07ae3c4bfda2e6
Tin <93732406+Tin0709@users.noreply.github.com>
2023-12-03 17:21:03 +0700

    OMW-125

--- src/routes/list-view-bookmarks/+page.svelte
+++ src/routes/list-view-bookmarks/+page.svelte
@@ -1,5 +1,19 @@
 <script>
 	import Bg from '$lib/components/backgrounds/BG.svelte';
+	import Headline from '$lib/components/typography/Headline.svelte';
+	import Button from '$lib/components/controls/Button.svelte';
+	import Icon from '$lib/components/iconography/Icon.svelte';
+	export let number = 0;
 </script>
 
 <Bg />
+<Icon
+	icon="faStar"
+	type="solid"
+	class=" absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 !text-9xl text-white/10"
+></Icon>
+
+<Headline class="text-decoration-line: underline  underline-offset-8 pt-8 pb-6">Bookmarks</Headline>
+
+<Headline class="text-white  text-opacity-50">{number} item{number > 1 ? 's' : ''}</Headline>
+<Button icon="faImage" class="mt-10">Edit items</Button>
