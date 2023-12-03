3bb667f4758dc8bc4e78adfd3b7a9e01a2881914
Tin <93732406+Tin0709@users.noreply.github.com>
2023-12-03 16:21:05 +0700

    OMW-119

--- src/routes/list-view/+page.svelte
+++ src/routes/list-view/+page.svelte
@@ -3,14 +3,16 @@
 	import Headline from '$lib/components/typography/Headline.svelte';
 	import BodyLarge from '$lib/components/typography/BodyLarge.svelte';
 	import Button from '$lib/components/controls/Button.svelte';
+	export let number = 0;
 </script>
 
-<Bg randomized></Bg>
+<Bg></Bg>
 
 <Headline class="text-decoration-line: underline  underline-offset-8 pt-8 pb-6"
 	>My list with a lovingly long name</Headline
 >
-<Headline class="text-white  text-opacity-50">24 Items</Headline>
+
+<Headline class="text-white  text-opacity-50">{number} item{number > 1 ? 's' : ''}</Headline>
 <BodyLarge class="pt-6">This is a special collection in which I collect special things</BodyLarge>
 
 <div class="mt-10">
