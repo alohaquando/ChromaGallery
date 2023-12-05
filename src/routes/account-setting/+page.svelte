162aea058f4cabdfec315ecb4233f71e9b1b5957
Tin <93732406+Tin0709@users.noreply.github.com>
2023-12-05 14:11:54 +0700

    OMW-156

---
+++ src/routes/account-setting/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import Modal from '$lib/components/pop-up/Modal.svelte';
+	import ListItem from '$lib/components/item/ListItem.svelte';
+	import Item from '$lib/wip/backend/Item.svelte';
+
+	export let data: Item = {};
+</script>
+
+<Modal title="Account setting">
+	<div class=" flex flex-col justify-center items-center">
+		<ListItem
+			leadingText="Update name"
+			icon="faChevronRight"
+			topDivider={false}
+			bottomDivider={false}
+			clickable={true}
+			data={Item}
+		/>
+		<ListItem
+			leadingText="Update email"
+			icon="faChevronRight"
+			topDivider={false}
+			bottomDivider={false}
+			clickable={true}
+			data={Item}
+		/>
+		<ListItem
+			leadingText="Update password"
+			icon="faChevronRight"
+			topDivider={false}
+			bottomDivider={false}
+			clickable={true}
+			data={Item}
+		/>
+		<ListItem
+			leadingText="Log out"
+			design="destructive"
+			topDivider={false}
+			bottomDivider={false}
+			clickable={true}
+			data={Item}
+		/>
+	</div>
+</Modal>
