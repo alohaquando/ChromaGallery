21d5393c091ba3ae55ecffe5770e7dc5c0723646
Tin <93732406+Tin0709@users.noreply.github.com>
2023-12-05 14:38:44 +0700

    OMW-159

---
+++ src/routes/account-update-name/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Modal from '$lib/components/pop-up/Modal.svelte';
+	import TextField from '$lib/components/inputs/TextField.svelte';
+	import BodyLarge from '$lib/components/typography/BodyLarge.svelte';
+</script>
+
+<Modal title="Update name" buttonText="Update">
+	<div class=" flex flex-col justify-center gap-4 pb-8">
+		<BodyLarge>New name</BodyLarge>
+		<TextField placeholder="New name"></TextField>
+	</div>
+</Modal>
