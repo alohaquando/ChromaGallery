ac986e77a7d250cae5c80cb248830ec62fc8f723
Tin <93732406+Tin0709@users.noreply.github.com>
2023-12-06 18:13:47 +0700

    tg6gt

--- src/routes/search-in-edit-list-items/+page.svelte
+++ src/routes/search-in-edit-list-items/+page.svelte
@@ -1,18 +1,15 @@
-<script>
+<script lang="ts">
 	import Modal from '$lib/components/pop-up/Modal.svelte';
 	import TextField from '$lib/components/inputs/TextField.svelte';
 	import RowItem from '$lib/components/item/RowItem.svelte';
+	import DragList from '$lib/components/item/DragList.svelte';
+	import { itemList } from '../../data.js';
 </script>
 
-<Modal title="Edit items" buttonText="Save">
+<Modal buttonText="Save" title="Edit items">
 	<div class="flex justify-center items-center flex-col gap-6">
-		<TextField></TextField>
-		<div class="flex flex-col justify-center items-center w-full gap-4">
-			<RowItem type="edit"></RowItem>
-			<RowItem type="edit"></RowItem>
-			<RowItem type="edit"></RowItem>
-			<RowItem type="edit"></RowItem>
-			<RowItem type="edit"></RowItem>
-		</div>
+		<TextField id="1" name="1" placeholder="q"></TextField>
+		<DragList class="" data={itemList}></DragList>
+
 	</div>
 </Modal>
