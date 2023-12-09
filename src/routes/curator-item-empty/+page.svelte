7d6ae1a6388f60f47dacdf359a1360639bdca031
Tin <93732406+Tin0709@users.noreply.github.com>
2023-12-09 22:56:02 +0700

    OMW-183

---
+++ src/routes/curator-item-empty/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Bg from '$lib/components/backgrounds/BG.svelte';
+	import Button from '$lib/components/controls/Button.svelte';
+	import PageTitle from '$lib/components/layouts/PageTitle.svelte';
+	import Fab from '$lib/components/controls/Fab.svelte';
+
+</script>
+
+<Bg randomized />
+
+<PageTitle>Items</PageTitle>
+<Button class="mb-10 mt-20" icon="faMagnifyingGlass" width="full">Search</Button>
+
+<div class="text-center text-white text-opacity-50 text-base font-normal font-['Atkinson Hyperlegible'] leading-normal">
+	No item added yet
+</div>
+
+<Fab class="fixed bottom-[15%] right-6" icon="faPlus"></Fab>
