eddc6ab20511c5739cefd9e36959054be23d4e1c
laystr <bomby88888@gmail.com>
2023-12-09 19:44:11 +0700

    Search page layouts

---
+++ src/routes/search-result-yes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Bg from '$lib/components/backgrounds/BG.svelte';
+	import TextField from '$lib/components/inputs/TextField.svelte';
+    import DragList from '$lib/components/item/DragList.svelte';
+	import { allItem } from '$lib/../data';
+    let items= allItem;
+</script>
+
+<Bg randomized/>
+
+<div class="mb-6 w-full h-full">
+<TextField id="" name="" placeholder="Text"></TextField>
+</div>
+
+<DragList data={items} type="view" class="gap-4"></DragList>
---
+++ src/routes/search/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Bg from '$lib/components/backgrounds/BG.svelte';
+	import TextField from '$lib/components/inputs/TextField.svelte';
+    import ListItem from '$lib/components/item/ListItem.svelte';
+</script>
+
+<!--<div class="w-screen h-screen backdrop-blur-sm fixed"></div>-->
+<Bg randomized/>
+
+<div class="mt-6 w-full h-full">
+<TextField id="" name="" placeholder="Text"></TextField>
+
+<div class="mt-6">
+    <ListItem leadingText="Suggestion 1" icon="faArrowUpLeft" topDivider={false} bottomDivider={false}/>
+    <ListItem leadingText="Suggestion 2" icon="faArrowUpLeft" topDivider={false} bottomDivider={false}/>
+    <ListItem leadingText="Suggestion 3" icon="faArrowUpLeft" topDivider={false} bottomDivider={false}/>
+</div>
+</div>
