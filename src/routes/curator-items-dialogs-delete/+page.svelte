254144e77293b85342ded6b24ab9aaa82ab00886
Tin <93732406+Tin0709@users.noreply.github.com>
2023-12-10 14:58:04 +0700

    OMW-192

---
+++ src/routes/curator-items-dialogs-delete/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import Dialog from '$lib/components/pop-up/Dialog.svelte';
+	import { item1 } from '../../data';
+</script>
+
+<Dialog option1="Keep" option2="Delete" text="This cannot be undone" title='Delete “{item1.name}”'></Dialog>
