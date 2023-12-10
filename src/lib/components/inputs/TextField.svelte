561c461cdd118eb727a989715f1a72deff18b0d2
Shrimpy <s3978724@rmit.edu.vn>
2023-12-10 15:46:25 +0700

    OMW-195

--- src/lib/components/inputs/TextField.svelte
+++ src/lib/components/inputs/TextField.svelte
@@ -1,80 +1,91 @@
 <script lang="ts">
-	import Icon from '$lib/components/iconography/Icon.svelte';
+    import Icon from '$lib/components/iconography/Icon.svelte';
+    import { DateInput } from 'date-picker-svelte';
 
-	export let id: string;
-	export let placeholder: string;
-	export let name: string;
-	export let label: string | undefined = undefined;
-	export let error: boolean = false;
-	export let errorMessage: string | undefined = 'Please check this input again';
-	export let type:
-		| 'color'
-		| 'date'
-		| 'datetime-local'
-		| 'email'
-		| 'month'
-		| 'number'
-		| 'password'
-		| 'range'
-		| 'reset'
-		| 'search'
-		| 'tel'
-		| 'text'
-		| 'time'
-		| 'url'
-		| 'week' = 'text';
-	export let required: boolean = false;
-	export let disabled: boolean = false;
-	export let readonly: boolean = false;
-	export let autocomplete: string | null | undefined = undefined;
-	export let autofocus: boolean | null | undefined = undefined;
-	export let form: string | undefined = undefined;
-	export let min: number | any | undefined = undefined;
-	export let max: number | any | undefined = undefined;
-	export let minlength: number | undefined = undefined;
-	export let maxlength: number | undefined = undefined;
-	export let pattern: any | undefined = undefined;
-	export let size: number | undefined = undefined;
-	export let value: string | undefined | null = null;
+    let date = new Date();
+    export let id: string;
+    export let placeholder: string;
+    export let name: string;
+    export let label: string | undefined = undefined;
+    export let error: boolean = false;
+    export let errorMessage: string | undefined = 'Please check this input again';
+    export let type:
+        | 'color'
+        | 'date'
+        | 'datetime-local'
+        | 'email'
+        | 'month'
+        | 'number'
+        | 'password'
+        | 'range'
+        | 'reset'
+        | 'search'
+        | 'tel'
+        | 'text'
+        | 'time'
+        | 'url'
+        | 'week' = 'text';
+    export let required: boolean = false;
+    export let disabled: boolean = false;
+    export let readonly: boolean = false;
+    export let autocomplete: string | null | undefined = undefined;
+    export let autofocus: boolean | null | undefined = undefined;
+    export let form: string | undefined = undefined;
+    export let min: number | any | undefined = undefined;
+    export let max: number | any | undefined = undefined;
+    export let minlength: number | undefined = undefined;
+    export let maxlength: number | undefined = undefined;
+    export let pattern: any | undefined = undefined;
+    export let size: number | undefined = undefined;
+    export let value: string | Date | undefined | null = null;
 
-	let stateClasses: string;
-	if (error) {
-		stateClasses = '!border-red-300';
-	}
+    switch (type) {
+        case 'date':
+            value = date;
+            disabled = true;
+    }
+
+    let stateClasses: string;
+    if (error) {
+        stateClasses = '!border-red-300';
+    }
 </script>
 
 <div class="flex flex-col w-full">
-	{#if label}
-		<label class="block mb-2 text-sm font-sans trim-both pb-4" for={id}>{label}</label>
-	{/if}
-	<input
-		{autocomplete}
-		{autofocus}
-		class="{stateClasses} bg-black/30 border border-white/30 rounded-xl placeholder-white/50 block w-full p-2.5 transition outline-none text-white font-sans trim-both
+    {#if label}
+        <label class="block mb-2 text-sm font-sans trim-both pb-4" for={id}>{label}</label>
+    {/if}
+    <input
+            {autocomplete}
+            {autofocus}
+            bind:value
+            class="{stateClasses} bg-black/30 border border-white/30 rounded-xl placeholder-white/50 block w-full p-2.5 transition outline-none text-white font-sans trim-both
 		focus:ring-white focus:ring-2
 		disabled:placeholder-white/30 disabled:text-white/70
 		read-only:focus:ring-0 read-only:text-white/70"
-		{disabled}
-		{form}
-		{id}
-		{max}
-		{maxlength}
-		{min}
-		{minlength}
-		{name}
-		on:change
-		on:input
-		{pattern}
-		{placeholder}
-		{readonly}
-		{required}
-		{size}
-		bind:value
-	/>
-	{#if error}
-		<div class="flex space-x-2 items-center text-red-300 pt-4">
-			<Icon icon="faExclamationCircle" />
-			<label class="block mb-2 text-sm font-sans trim-both" for={id}>{errorMessage}</label>
-		</div>
-	{/if}
+            {disabled}
+            {form}
+            {id}
+            {max}
+            {maxlength}
+            {min}
+            {minlength}
+            {name}
+            on:change
+            on:input
+            {pattern}
+            {placeholder}
+            {readonly}
+            {required}
+            {size}
+    />
+    {#if type === 'date'}
+        <DateInput bind:value={date}></DateInput>
+    {/if}
+    {#if error}
+        <div class="flex space-x-2 items-center text-red-300 pt-4">
+            <Icon icon="faExclamationCircle" />
+            <label class="block mb-2 text-sm font-sans trim-both" for={id}>{errorMessage}</label>
+        </div>
+    {/if}
 </div>
--- src/routes/test-kiet/+page.svelte
+++ src/routes/test-kiet/+page.svelte
@@ -1,24 +1,27 @@
 <script lang="ts">
-	import CollectionBlock from '$lib/components/item/CollectionBlock.svelte';
-	import Link from '$lib/components/links/Link.svelte';
-	import Body from '$lib/components/typography/Body.svelte';
-	import HeroImage from '$lib/components/item/HeroImage.svelte';
-	import Dialog from '$lib/components/pop-up/Dialog.svelte';
-	import Block from '$lib/components/item/Block.svelte';
-	import Modal from '$lib/components/pop-up/Modal.svelte';
-	import Button from '$lib/components/controls/Button.svelte';
-	import Socials from '$lib/components/socials/Socials.svelte';
-	import PageTitle from '$lib/components/layouts/PageTitle.svelte';
-	import Grid from '$lib/components/item/GridItem.svelte';
-	import RowItem from '$lib/components/item/RowItem.svelte';
-	import BG from '$lib/components/backgrounds/BG.svelte';
-	import { item1, collection1 } from '../../data';
-	import FileInput from '$lib/components/inputs/FileInput.svelte';
+    import CollectionBlock from '$lib/components/item/CollectionBlock.svelte';
+    import Link from '$lib/components/links/Link.svelte';
+    import Body from '$lib/components/typography/Body.svelte';
+    import HeroImage from '$lib/components/item/HeroImage.svelte';
+    import Dialog from '$lib/components/pop-up/Dialog.svelte';
+    import Block from '$lib/components/item/Block.svelte';
+    import Modal from '$lib/components/pop-up/Modal.svelte';
+    import Button from '$lib/components/controls/Button.svelte';
+    import Socials from '$lib/components/socials/Socials.svelte';
+    import PageTitle from '$lib/components/layouts/PageTitle.svelte';
+    import Grid from '$lib/components/item/GridItem.svelte';
+    import RowItem from '$lib/components/item/RowItem.svelte';
+    import BG from '$lib/components/backgrounds/BG.svelte';
+    import { item1, collection1 } from '../../data';
+    import FileInput from '$lib/components/inputs/FileInput.svelte';
+    import TextField from '$lib/components/inputs/TextField.svelte';
 </script>
 
 <BG />
 
-<FileInput></FileInput>
+<TextField type="date"></TextField>
+
+<!--<FileInput></FileInput>-->
 
 <!--<PageTitle>A Title</PageTitle>-->
 <!--<HeroImage data={item1}></HeroImage>-->
