3cba9547b9375604428643dba11ecfc02571d439
laystr <bomby88888@gmail.com>
2023-12-10 14:28:31 +0700

    Clean up data code

--- src/routes/search-result-yes/+page.svelte
+++ src/routes/search-result-yes/+page.svelte
@@ -2,8 +2,7 @@
 	import Bg from '$lib/components/backgrounds/BG.svelte';
 	import TextField from '$lib/components/inputs/TextField.svelte';
     import DragList from '$lib/components/item/DragList.svelte';
-	import { allItem } from '$lib/../data';
-    let items= allItem;
+	import {allItem} from '$lib/../data';
 </script>
 
 <Bg randomized/>
@@ -12,4 +11,4 @@
 <TextField id="" name="" placeholder="Text"></TextField>
 </div>
 
-<DragList data={items} type="view" class="gap-4"></DragList>
+<DragList data={allItem} type="view" class="gap-4"></DragList>
