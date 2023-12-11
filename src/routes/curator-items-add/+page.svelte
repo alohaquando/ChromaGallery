04356e4423c7cf1057fceb018dc699cc9fe4ecf8
Tin <93732406+Tin0709@users.noreply.github.com>
2023-12-11 15:57:03 +0700

    OMW-191

---
+++ src/routes/curator-items-add/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import Modal from '$lib/components/pop-up/Modal.svelte';
+	import FileInput from '$lib/components/inputs/FileInput.svelte';
+	import HeroImage from '$lib/components/item/HeroImage.svelte';
+	import TextField from '$lib/components/inputs/TextField.svelte';
+	import Divider from '$lib/components/layouts/Divider.svelte';
+
+	import { item2 } from '../../data';
+</script>
+
+<Modal button="Save" title="Edit item">
+	<div class="w-full flex-col flex justify-center">
+		<FileInput state='add'></FileInput>
+
+
+		<div class="w-full flex-col flex justify-center gap-6 mt-6">
+			<Divider></Divider>
+			<TextField id='' label="Name" name="" placeholder='' type="name"></TextField>
+			<TextField id='' label="Artist" name="" placeholder='' type="text"></TextField>
+			<TextField id='' label="Time" name="" placeholder='' type="date"></TextField>
+			<TextField id='' label="Description" name="" placeholder=''></TextField>
+
+		</div>
+
+		<div class="w-full flex-col flex justify-center gap-6 mt-8">
+			<Divider />
+			<TextField id='' label="Extra field 1" name="" placeholder=''></TextField>
+			<TextField id='' label="Extra field 2" name="" placeholder=''></TextField>
+		</div>
+
+	</div>
+
+</Modal>
