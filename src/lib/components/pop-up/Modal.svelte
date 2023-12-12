a209d7f39e596a50c67e51365efb462ff130e173
Quân Đỗ <aloha.quando@gmail.com>
2023-12-12 18:56:27 +0700

    OMW-198 Updated modal style

--- src/lib/components/pop-up/Modal.svelte
+++ src/lib/components/pop-up/Modal.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
 	import HeaderBack from '$lib/components/navigation/HeaderBack.svelte';
 	import Headline from '$lib/components/typography/Headline.svelte';
-	import { afterNavigate, beforeNavigate } from '$app/navigation';
-	import { modal, previousState } from '$lib/stores/modal';
-	import { onMount } from 'svelte';
 
 	let link: string | undefined;
 	export { link as href };
@@ -14,17 +11,18 @@
 	};
 	export let animation = '';
 
-	let customClass = '';
-	export { customClass as class };
+
 </script>
 
 <div
-	class="{customClass} {animation} relative top-5 z-40 w-screen h-[95vh] backdrop-blur-md bg-gradient-to-b from-neutral-900 via-black/40 via-90% to-black/0 flex border-t-4 border-white/20 flex-col"
+	class=" {animation} relative top-5 z-40 w-screen h-[95vh] backdrop-blur-md bg-gradient-to-b from-neutral-900 via-black/40 via-90% to-black/0 flex rounded-t-3xl overflow-clip flex-col"
 >
 	<HeaderBack {button} {buttonFunction} {exit} href="{link}" isRelative
 							responsive={false}></HeaderBack>
-	<Headline class="px-6 mt-4">{title}</Headline>
-	<div class="mt-12 px-6">
-		<slot />
+	<div class="container mx-auto px-6">
+		<Headline class=" mt-4">{title}</Headline>
+		<div class="mt-12">
+			<slot />
+		</div>
 	</div>
 </div>
