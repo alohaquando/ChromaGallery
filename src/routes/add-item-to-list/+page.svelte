13277b01eacbb5438f1c7487dcc381543154c96c
Tin <93732406+Tin0709@users.noreply.github.com>
2023-12-14 20:54:39 +0700

    add-item-to-list

---
+++ src/routes/add-item-to-list/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Modal from '$lib/components/pop-up/Modal.svelte';
+	import Button from '$lib/components/controls/Button.svelte';
+	import DragList from '$lib/components/item/DragList.svelte';
+
+	import { itemList1 } from '../../data';
+</script>
+
+<Modal button="Add" title="Add item to list">
+	<div class="flex justify-center items-center flex-col gap-6">
+		<Button class="w-full" icon="faSearch">Find list</Button>
+		<DragList class="w-full gap-4 " data={itemList1}></DragList>
+	</div>
+</Modal>
