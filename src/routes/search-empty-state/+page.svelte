c4c614287b51679acf11d37f291a2c5dbca3c42f
laystr <bomby88888@gmail.com>
2023-12-19 17:32:10 +0700

    Added x icon at the end of search bar to empty stage page

---
+++ src/routes/search-empty-state/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Bg from '$lib/components/backgrounds/BG.svelte';
+	import TextField from '$lib/components/inputs/TextField.svelte';
+    import BodySmall from '$lib/components/typography/BodySmall.svelte';
+</script>
+
+<Bg randomized/>
+
+<div class="w-full h-full">
+<TextField id="" name="" value="Text" icon="faXmark"></TextField>
+</div>
+
+<div class="mt-12 flex flex-col items-center w-full">
+    <BodySmall>No result found</BodySmall>
+</div>
