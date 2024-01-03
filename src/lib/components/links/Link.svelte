f24c30d2bcf5f50afe9edd1322ae3b6039516c83
Shrimpy <s3978724@rmit.edu.vn>
2024-01-03 23:35:38 +0700

    fix link animation bug

--- src/lib/components/links/Link.svelte
+++ src/lib/components/links/Link.svelte
@@ -26,39 +26,43 @@
 			type: 'body',
 			component: Body,
 			class: 'bg-[length:0_1.5px]',
-			display: 'bg-[length:100%_1.5px]'
+			static: 'bg-[length:100%_1.5px]',
+			hover: 'hover:bg-[length:100%_1.5px]'
 		},
 		{
 			type: 'title',
 			component: Title,
 			class: 'bg-[length:0_1.5px]',
-			display: 'bg-[length:100%_1.5px]'
+			static: 'bg-[length:100%_1.5px]',
+			hover: 'hover:bg-[length:100%_1.5px]'
 		},
 		{
 			type: 'headline',
 			component: Headline,
 			class: 'bg-[length:0_3px]',
-			display: 'bg-[length:100%_3px]'
+			static: 'bg-[length:100%_3px]',
+			hover: 'hover:bg-[length:100%_3px]'
 		},
 		{
 			type: 'display',
 			component: Display,
 			class: 'bg-[length:0_4px]',
-			display: 'bg-[length:100%_4px]'
+			static: 'bg-[length:100%_4px]',
+			hover: 'hover:bg-[length:100%_4px]'
 		}
 	];
 
 	let component = componentOptions.find((option) => option.type == type)?.component;
 	let underlineClass = componentOptions.find((option) => option.type == type)?.class;
-	let display = componentOptions.find((option) => option.type == type)?.display;
+	let staticClass = componentOptions.find((option) => option.type == type)?.static;
+	let hoverClass = componentOptions.find((option) => option.type == type)?.hover;
 
 	export let isHovered: boolean | undefined;
-	let displayClass = isHovered ? display : 'hover:' + display;
 </script>
 
 <a class="{customClasses} {disableClass} relative max-w-fit cursor-pointer trim-both" {href}>
 	<svelte:component
-		class="{underlineClass} {displayClass} bg-no-repeat bg-left-bottom duration-500 inline bg-gradient-to-r {linkColor}"
+		class="{underlineClass} {isHovered? staticClass : hoverClass} bg-no-repeat bg-left-bottom duration-500 inline bg-gradient-to-r {linkColor}"
 		this={component}>
 		<slot />
 	</svelte:component>
