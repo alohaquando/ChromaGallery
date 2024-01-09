30afc2c8731596fe85a6ec67b74d4882ca5ee0f5
Quân Đỗ <aloha.quando@gmail.com>
2024-01-09 19:55:22 +0700

    Updated Home page

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,11 +1,11 @@
 import { getAllItems, getFeaturedItems } from '$lib/data/item';
 
-export const load = () => {
+export const load = async () => {
 	return {
-		featuredItems: getFeaturedItems(),
-		allItems: getAllItems(),
 		background: {
 			toggled: false
-		}
+		},
+		featuredItems: await getFeaturedItems(),
+		allItems: await getAllItems()
 	};
 };
