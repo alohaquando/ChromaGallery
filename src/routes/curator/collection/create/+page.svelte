7e44a8723c0ded261db6a5c33fab5c7f3b723955
Shrimpy <s3978724@rmit.edu.vn>
2024-01-09 20:17:57 +0700

    update

--- src/routes/curator/collection/create/+page.svelte
+++ src/routes/curator/collection/create/+page.svelte
@@ -1,53 +1,60 @@
 <script lang="ts">
-	import TextArea from '$lib/components/inputs/TextArea.svelte';
-	import ListItem from '$lib/components/item/ListItem.svelte';
-	import TextField from '$lib/components/inputs/TextField.svelte';
-	import CollectionList from '$lib/components/item/CollectionList.svelte';
-	import Button from '$lib/components/controls/Button.svelte';
-	import { onMount } from 'svelte';
-	import { enhance } from '$app/forms';
-	import type { SubmitFunction } from '@sveltejs/kit';
-	import { uploadFileGetUrl } from '$lib/data/item';
+    import TextArea from '$lib/components/inputs/TextArea.svelte';
+    import ListItem from '$lib/components/item/ListItem.svelte';
+    import TextField from '$lib/components/inputs/TextField.svelte';
+    import CollectionList from '$lib/components/item/CollectionList.svelte';
+    import Button from '$lib/components/controls/Button.svelte';
+    import { onMount } from 'svelte';
+    import { enhance } from '$app/forms';
+    import type { SubmitFunction } from '@sveltejs/kit';
+    import { uploadFileGetUrl } from '$lib/data/item';
+    import FormError from '$lib/components/inputs/FormError.svelte';
 
-	let isLoading = false;
+    let isLoading = false;
+    export let form;
 
-	const handleSubmit: SubmitFunction = async ({ formData }) => {
-		isLoading = true;
+    const handleSubmit: SubmitFunction = async ({ formData }) => {
+        isLoading = true;
 
-		return async ({ update }) => {
-			await update();
-			isLoading = false;
-		};
-	};
+        return async ({ update }) => {
+            await update();
+            isLoading = false;
+        };
+    };
 </script>
 
 
-	<form
-		action="?/add"
-		class="w-full flex-col flex justify-center gap-10 mt-6"
-		enctype="multipart/form-data"
-		method="POST"
-		use:enhance={handleSubmit}>
-		<div class="gap-8 flex flex-col">
+<form
+        action="?/add"
+        class="w-full flex-col flex justify-center gap-10 mt-6"
+        enctype="multipart/form-data"
+        method="POST"
+        use:enhance={handleSubmit}>
+    <div class="gap-8 flex flex-col">
 
-	<div class="w-full">
-		<TextField id="create" label="Name" labelSize="lg" name="title" placeholder="Collection name"
-		></TextField>
-	</div>
+        <div class="w-full">
+            <TextField id="create" label="Name" labelSize="lg" name="title" placeholder="Collection name"
+            ></TextField>
+        </div>
 
-	<div class="w-full">
+        <div class="w-full">
 		<TextArea
-			id="create"
-			label="Description"
-			labelSize="lg"
-			name="desc"
-			placeholder="Collection description"
-			rows={2}
-		></TextArea>
-	</div>
-			<Button disabled={isLoading} sticky type="submit">
-				{isLoading ? 'Loading...' : 'Add'}
-			</Button>
+                id="create"
+                label="Description"
+                labelSize="lg"
+                name="desc"
+                placeholder="Collection description"
+                rows={2}
+        ></TextArea>
+        </div>
 
-</div>
+        {#if form}
+            <FormError>{form.message}</FormError>
+        {/if}
+
+        <Button disabled={isLoading} sticky type="submit">
+            {isLoading ? 'Loading...' : 'Add'}
+        </Button>
+
+    </div>
 </form>
