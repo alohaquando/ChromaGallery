c2360e71caedd7413d59f6bd14e171e43a3a68c8
Shrimpy <s3978724@rmit.edu.vn>
2024-01-10 18:14:30 +0700

    update

--- src/lib/components/item/ImageView.svelte
+++ src/lib/components/item/ImageView.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
-    import { imageFullView, resetImageFullView } from '$lib/stores/imageFullView';
-    import HeaderBack from '$lib/components/navigation/HeaderBack.svelte';
+	import { imageFullView, resetImageFullView } from '$lib/stores/imageFullView';
+	import HeaderBack from '$lib/components/navigation/HeaderBack.svelte';
 
-    export let src = '';
-    export let alt = '';
+	export let src = '';
+	export let alt = '';
 </script>
 
 {#if $imageFullView.toggled}
-    <HeaderBack buttonClass="bg-neutral-900/30 !border-neutral-700/50 hover:!border-neutral-700/80 text-black/50"
-                class="!absolute top-0 left-0 z-50 !blur-0"
-                exit
-                responsive={false}
-                exitButton={resetImageFullView}></HeaderBack>
-    <div class="w-full h-full top-0 left-0 fixed bg-black z-40 flex justify-center items-center">
+	<HeaderBack
+		buttonClass="bg-neutral-700 !border-neutral-500 hover:!border-neutral-700 !text-neutral-300 hover:!text-black"
+		class="!absolute top-0 left-0 z-50 !blur-0"
+		exit
+		responsive={false}
+		exitButton={resetImageFullView}></HeaderBack>
+	<div class="w-full h-full top-0 left-0 fixed bg-black z-40 flex justify-center items-center">
 
-        <img {alt} class="w-full max-h-full object-contain" {src} />
-    </div>
+		<img {alt} class="w-full max-h-full object-contain" {src} />
+	</div>
 {/if}
